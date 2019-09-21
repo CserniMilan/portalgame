eac13def745a394897571bc66ebe3e370ce94c93
csernimilan <csernimm@gmail.com>
2019-09-21 12:33:59 +0200

    Create project and some scripts

---
+++ Portalgame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float screenWidth = Screen.width; //camel case
+    private float addSpeed; //pascal case
+
+    void Start()
+    {
+        addSpeed = 0;
+    }
+
+    void Update()    {
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -1.5)
+            transform.position -= Vector3.right * 1f;
+        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 1.5)
+            transform.position -= Vector3.right * 1f;
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (Input.GetTouch(i).position.x < screenWidth / 2
+                 && Input.GetTouch(i).phase == TouchPhase.Ended
+                 && transform.position.x > -1.5f)
+            {
+                transform.position -= Vector3.right * 1f;
+            }
+
+            if (Input.GetTouch(i).position.x < screenWidth / 2
+                 && Input.GetTouch(i).phase == TouchPhase.Ended
+                 && transform.position.x < 1.5f)
+            {
+                transform.position += Vector3.right * 1f;
+            }
+        }
+    }
+}
---
+++ Portalgame/Assets/Scripts/RandomMatGen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMatGen : MonoBehaviour
+{
+    public GameObject[] other;
+
+    void Start()
+    {
+        List<Color> color = new List<Color>() { Color.red, Color.green, Color.yellow };
+        color.Add(Color.white);
+
+        if(transform.name == "player")
+        {;
+            Color randomColor = color[Random.Range(0, color.Count)];
+            GetComponent<Renderer>().material.color = randomColor;
+        }
+        if(transform.childCount != 0)
+        {
+            foreach(GameObject item in other)
+            {
+                Color randomColor = color[Random.Range(0, color.Count)];
+                item.GetComponent<Renderer>().material.color = randomColor;
+                color.Remove(randomColor);
+            }
+        }
+        
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
